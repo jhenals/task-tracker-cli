548acb7bcfae391a2a1e9c0ba20892ecec93b3ae
Jhenals <suboljhenalyn3@gmail.com>
2025-02-27 19:30:27 +0100

    refactor TaskManager.java

--- src/components/TaskManager.java
+++ src/components/TaskManager.java
@@ -154,8 +154,3 @@ public class TaskManager {
 
 
 }
-
-
-/*
-java.Task Tracker is a simple CLI application for managing tasks that helps users to track what they need to do, what they're working on, and what they've completed.
- */
